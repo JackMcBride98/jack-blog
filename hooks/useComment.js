513b90a3362fdde9cbe48643dd9b3bb3c6c98cc9
Jack McBride <mcbride.jack1@gmail.com>
2022-02-20 15:08:21 +0000

    ran npm install (had some warnings), fixed comments by removing comment id from parent and recursively deleteing replies

--- hooks/useComment.js
+++ hooks/useComment.js
@@ -43,7 +43,6 @@ export default function useComments() {
 
   const onDelete = async (comment) => {
     const token = await getAccessTokenSilently()
-    console.log(comment)
 
     try {
       await fetch('/api/comment', {
